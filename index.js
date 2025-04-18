7126aed58b9d76dded157d71058d2be5c339fb19
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2025-04-17 23:59:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,43 @@
+console.log('Iniciando🌸') 
 import { join, dirname } from 'path'
-import { createRequire } from 'module';
+import { createRequire } from 'module'
 import { fileURLToPath } from 'url'
 import { setupMaster, fork } from 'cluster'
 import { watchFile, unwatchFile } from 'fs'
-import cfonts from 'cfonts';
+import cfonts from 'cfonts'
 import { createInterface } from 'readline'
 import yargs from 'yargs'
-import express from 'express'
-import chalk from 'chalk'
-import path from 'path'
-import os from 'os'
-import { promises as fsPromises } from 'fs'
-
-// https://stackoverflow.com/a/50052194
 const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname) //Incorpora la capacidad de crear el método 'requerir'
-const { name, author } = require(join(__dirname, './package.json')) //https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
+const require = createRequire(__dirname)
+const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
-
-//const app = express()
-//const port = process.env.PORT || 8080;
-
-say('MomoAyase\nBot\nMD', {
+ 
+say('KoharuBot-MD', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Por @Alba070503`, {
+say(`by: @Alba070503`, {
 font: 'console',
 align: 'center',
-gradient: ['red', 'magenta']})
+gradient: ['red', 'magenta']});
 
-var isRunning = false
-
-process.on('uncaughtException', (err) => {
-if (err.code === 'ENOSPC') {
-console.error('Se ha detectado ENOSPC (sin espacio o límite de watchers alcanzado), reiniciando....')
-} else {
-console.error('Error no capturado:', err)
-}
-process.exit(1)
-})
-
-async function start(file) {
+let isRunning = false
+/**
+* Start a js file
+* @param {String} file `path/to/file`
+*/
+function start(file) {
 if (isRunning) return
 isRunning = true
-const currentFilePath = new URL(import.meta.url).pathname
-let args = [join(__dirname, file), ...process.argv.slice(2)]
-say([process.argv[0], ...args].join(' '), {
-font: 'console',
-align: 'center',
-gradient: ['red', 'magenta']
-})
-setupMaster({exec: args[0], args: args.slice(1),
-})
-let p = fork()
-p.on('message', data => {
+const args = [join(__dirname, file), ...process.argv.slice(2)]
+
+setupMaster({
+exec: args[0],
+args: args.slice(1)})
+const p = fork()
+p.on('message', (data) => {
+//console.log('╭--------- - - - ✓\n┆ ✅ TIEMPO DE ACTIVIDAD ACTUALIZADA\n╰-------------------- - - -', data)
 switch (data) {
 case 'reset':
 p.process.kill()
@@ -65,58 +47,32 @@ break
 case 'uptime':
 p.send(process.uptime())
 break
-}})
-
-p.on('exit', (_, code) => {
-isRunning = false
-console.error('⚠️ ERROR ⚠️ >> ', code)
-start('main.js'); //
-
-if (code === 0) return
-watchFile(args[0], () => {
-unwatchFile(args[0])
-start(file)
-})})
-
-const ramInGB = os.totalmem() / (1024 * 1024 * 1024)
-const freeRamInGB = os.freemem() / (1024 * 1024 * 1024)
-const packageJsonPath = path.join(path.dirname(currentFilePath), './package.json')
-try {
-const packageJsonData = await fsPromises.readFile(packageJsonPath, 'utf-8')
-const packageJsonObj = JSON.parse(packageJsonData)
-const currentTime = new Date().toLocaleString()
-let lineM = '⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ 》'
-console.log(chalk.yellow(`╭${lineM}
-┊${chalk.blueBright('╭┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}
-┊${chalk.blueBright('┊')}${chalk.yellow(`🖥️ ${os.type()}, ${os.release()} - ${os.arch()}`)}
-┊${chalk.blueBright('┊')}${chalk.yellow(`💾 Total RAM: ${ramInGB.toFixed(2)} GB`)}
-┊${chalk.blueBright('┊')}${chalk.yellow(`💽 Free RAM: ${freeRamInGB.toFixed(2)} GB`)}
-┊${chalk.blueBright('╰┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}
-┊${chalk.blueBright('╭┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}
-┊${chalk.blueBright('┊')} ${chalk.blue.bold(`🟢INFORMACIÓN :`)}
-┊${chalk.blueBright('┊')} ${chalk.blueBright('┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')} 
-┊${chalk.blueBright('┊')}${chalk.cyan(`💚 Nombre: ${packageJsonObj.name}`)}
-┊${chalk.blueBright('┊')}${chalk.cyan(`𓃠 Versión: ${packageJsonObj.version}`)}
-┊${chalk.blueBright('┊')}${chalk.cyan(`💜 Descripción: ${packageJsonObj.description}`)}
-┊${chalk.blueBright('┊')}${chalk.cyan(`😺 Project Author: ${packageJsonObj.author.name} Powered @Alba070503`)}
-┊${chalk.blueBright('┊')}${chalk.blueBright('┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')} 
-┊${chalk.blueBright('┊')}${chalk.yellow(`💜 Colaborador:`)}
-┊${chalk.blueBright('┊')}${chalk.yellow(`• GataDios`)}
-┊${chalk.blueBright('╰┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')} 
-┊${chalk.blueBright('╭┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}
-┊${chalk.blueBright('┊')}${chalk.cyan(`⏰ Hora Actual :`)}
-┊${chalk.blueBright('┊')}${chalk.cyan(`${currentTime}`)}
-┊${chalk.blueBright('╰┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')} 
-╰${lineM}`));
-setInterval(() => {}, 1000)
-} catch (err) {
-console.error(chalk.red(`❌ No se pudo leer el archivo package.json: ${err}`))
 }
-
-let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
-if (!opts['test'])
-if (!rl.listenerCount()) rl.on('line', line => {
+})
+p.on('exit', (_, code) => {
+isRunning = false;
+console.error('⚠️ Error Inesperado ⚠️', code)
+p.process.kill()
+isRunning = false
+start.apply(this, arguments)
+if (process.env.pm_id) {
+process.exit(1)
+} else {
+process.exit()
+}})
+const opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+if (!opts['test']) {
+if (!rl.listenerCount()) {
+rl.on('line', (line) => {
 p.emit('message', line.trim())
-})}
+})
+}}}
 
-start('main.js')
+setInterval(() => {
+console.log('♻️ Reiniciando bot automáticamente...');
+process.exit(0); 
+}, 14400000) //4hs
+
+start('main.js');
+
+
