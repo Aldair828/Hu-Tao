8c8d6c8adef75a98c0b09e7267d89e222cc763f5
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2025-04-19 23:51:29 -0400

    Update rpg-unreg.js

--- plugins/rpg-unreg.js
+++ plugins/rpg-unreg.js
@@ -1,16 +1,14 @@
-import { createHash } from 'crypto'
-let handler = async function (m, { conn, args, usedPrefix}) {
-  if (!args[0]) return m.reply(`🚩 Ingresa tu número de serie junto al comando.`)
-  let user = global.db.data.users[m.sender]
-  let sn = createHash('md5').update(m.sender).digest('hex')
-  if (args[0] !== sn) return m.reply('🚩 Número de serie incorrecto')
-  user.registered = false
-  m.reply(`🚩 Registro eliminado.`)
+let handler = async (m, { conn, text }) => {
+
+let user = global.db.data.users[m.sender]
+
+user.registered = false
+
+return conn.reply(m.chat, `🚩 Usted anuló su registro de ShizukaBot-MD 🌻✨️*\n\n🚩 Puede usar #reg nombre.edad para realizar un nuevo registro`, m)
+
 }
-handler.help = ['unreg'] 
+handler.help = ['unreg']
 handler.tags = ['rg']
-
-handler.command = ['unreg'] 
+handler.command = ['unreg', 'unregister']
 handler.register = true
-
 export default handler
