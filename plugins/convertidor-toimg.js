6a9b6a218766bc6ba96d77b34ed7bf71f43ad463
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2025-04-20 00:05:38 -0400

    Update convertidor-toimg.js

--- plugins/convertidor-toimg.js
+++ plugins/convertidor-toimg.js
@@ -1,17 +1,21 @@
-import {webp2png} from '../lib/webp2mp4.js';
-const handler = async (m, {conn, usedPrefix, command}) => {
-const notStickerMessage = `*⚠️ ${await tr("Responder a un sticker que desee convertir en imagen con el siguiente comando:")}* ${usedPrefix + command}`;
-if (!m.quoted) throw notStickerMessage;
-m.reply(`${await tr("Euu flaco")} 🥴\n\n> *${await tr("Convirtiendo tu Sticker a Imagen")} 🔄*`) 
-const q = m.quoted || m;
-const mime = q.mediaType || '';
-if (!/sticker/.test(mime)) throw notStickerMessage;
-const media = await q.download();
-const out = await webp2png(media).catch((_) => null) || Buffer.alloc(0);
-await conn.sendFile(m.chat, out, 'error.png', null, m,);
-};
-handler.help = ['toimg (reply)'];
-handler.tags = ['convertidor']
-handler.command = ['toimg', 'jpg', 'img'];
-handler.register = true
-export default handler;
+import { webp2png } from '../lib/webp2mp4.js'
+
+var handler = async (m, { conn, usedPrefix, command }) => {
+
+const notStickerMessage = `*Responda a una imagen*`
+if (!m.quoted) throw notStickerMessage 
+const q = m.quoted || m
+let mime = q.mediaType || ''
+if (!/sticker/.test(mime)) throw notStickerMessage
+let media = await q.download()
+let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
+await conn.sendFile(m.chat, out, 'error.png', null, m, rcanal)
+
+}
+handler.help = ['toimg']
+handler.tags = ['transformador']
+handler.command = ['toimg', 'jpg', 'jpge', 'png']
+
+handler.limit = true
+
+export default handler
