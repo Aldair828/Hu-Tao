c81b83dfff94936ae5b7a1e4a1d99f0e6eeeb469
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2025-04-20 00:08:17 -0400

    Update convertidor-toimg.js

--- plugins/convertidor-toimg.js
+++ plugins/convertidor-toimg.js
@@ -1,21 +1,22 @@
 import { webp2png } from '../lib/webp2mp4.js'
-
-var handler = async (m, { conn, usedPrefix, command }) => {
-
-const notStickerMessage = `*Responda a una imagen*`
-if (!m.quoted) throw notStickerMessage 
+let handler = async (m, { conn, usedPrefix, command }) => {
+const notStickerMessage = `🚩 Responde a un *Sticker.*`
+if (!m.quoted) return conn.reply(m.chat, notStickerMessage, m, rcanal)
 const q = m.quoted || m
 let mime = q.mediaType || ''
-if (!/sticker/.test(mime)) throw notStickerMessage
+if (!/sticker/.test(mime)) return conn.reply(m.chat, notStickerMessage, m, rcanal)
+await m.react('🕓')
+try {
 let media = await q.download()
 let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-await conn.sendFile(m.chat, out, 'error.png', null, m, rcanal)
-
-}
-handler.help = ['toimg']
-handler.tags = ['transformador']
-handler.command = ['toimg', 'jpg', 'jpge', 'png']
-
-handler.limit = true
+await conn.sendFile(m.chat, out, 'thumbnail.jpg', listo, m, rcanal)
+await m.react('✅')
+} catch {
+await m.react('✖️')
+}}
+handler.help = ['toimg *<sticker>*']
+handler.tags = ['sticker', 'tools']
+handler.command = ['toimg', 'jpg', 'aimg'] 
+handler.register = true
 
 export default handler
