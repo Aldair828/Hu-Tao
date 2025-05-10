03aa82ab539978709980494c8a00512e373b73a6
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2025-05-10 12:47:56 -0400

    Update convertidor-toanime.js

--- plugins/convertidor-toanime.js
+++ plugins/convertidor-toanime.js
@@ -8,17 +8,17 @@ const data = await q.download?.();
 const image = await uploadImage(data);
 try {
 const anime = `https://api.lolhuman.xyz/api/imagetoanime?apikey=${lolkeysapi}&img=${image}`;
-await conn.sendFile(m.chat, anime, 'error.jpg', null, m, null, fake);
+await conn.sendFile(m.chat, anime, 'error.jpg', null, m);
 m.react(`✅`) 
 } catch (i) {
 try {
 const anime2 = `https://api.zahwazein.xyz/photoeditor/jadianime?url=${image}&apikey=${keysxxx}`;
-await conn.sendFile(m.chat, anime2, 'error.jpg', null, m, null, fake);
+await conn.sendFile(m.chat, anime2, 'error.jpg', null, m);
 m.react(`✅`)  
 } catch (a) {
 try {
 const anime3 = `https://api.caliph.biz.id/api/animeai?img=${image}&apikey=caliphkey`;
-await conn.sendFile(m.chat, anime3, 'error.jpg', null, m, null, fake);
+await conn.sendFile(m.chat, anime3, 'error.jpg', null, m);
 m.react(`✅`)         
 } catch (e) {
 m.reply(`\`\`\`⚠️ ${await tr("OCURRIO UN ERROR")} ⚠️\`\`\`\n\n> *${await tr("Reporta el siguiente error a mi creador con el comando:")}* #report\n\n>>> ${e} <<<< `)      
